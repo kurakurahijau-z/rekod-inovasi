7bbb5152ba3f766bd25504b20b050d49300ab9f1
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-17 23:22:20 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.APP_CONFIG = {
+  BASE_URL: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
+  ALLOWED_DOMAIN: "pms.edu.my",
+  STORAGE_TOKEN: "appToken",
+  STORAGE_EMAIL: "email",
+  // Google client id (same as backend)
+  CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com"
+};
