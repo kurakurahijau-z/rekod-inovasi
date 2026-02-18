aaaea3fd9ef992d508d8f11bd26a5c5d9b06b9bd
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-18 19:47:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 // config.js
 window.APP_CONFIG = {
+  // WAJIB guna URL domain script.googleusercontent.com (yang ada user_content_key)
   API_BASE: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
+
   GOOGLE_CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com",
   ALLOWED_DOMAIN: "pms.edu.my"
 };
