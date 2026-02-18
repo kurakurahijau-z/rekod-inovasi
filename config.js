5bedbab9245ba2f5820597864cd1df1e2662c436
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-18 08:37:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.APP_CONFIG = {
   BASE_URL: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
-  CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com",
-  ALLOWED_DOMAIN: "pms.edu.my",
-  ADMIN_EMAIL: "faizal.rahman@pms.edu.my",
+  ALLOWED_DOMAIN: "@pms.edu.my",
   STORAGE_TOKEN: "appToken",
   STORAGE_EMAIL: "email",
-  STORAGE_ROLE: "role"
+  STORAGE_ROLE: "role",
+  // Google OAuth Client ID (Web)
+  CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com"
 };
