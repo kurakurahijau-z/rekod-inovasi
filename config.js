0769faac665d8af9f32580fd442b0bdf7ddc0ef2
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-18 15:10:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
 // config.js
 window.APP_CONFIG = {
-  BASE_URL: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
-  ALLOWED_DOMAIN: "pms.edu.my",
-
-  // localStorage keys
-  STORAGE_TOKEN: "appToken",
-  STORAGE_EMAIL: "email",
-  STORAGE_ROLE: "role",
-
-  // Google OAuth Client ID (Web)
-  CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjids e7bpl23m.apps.googleusercontent.com".replace(/\s+/g,""),
+  API_BASE: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
+  GOOGLE_CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com",
+  ALLOWED_DOMAIN: "pms.edu.my"
 };
