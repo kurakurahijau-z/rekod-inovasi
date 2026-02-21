c37a1892ac533350d85c0343355d643cc042ca4f
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-21 13:57:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 window.APP_CONFIG = {
-  // WAJIB guna URL domain script.googleusercontent.com (yang ada user_content_key)
-  API_BASE: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
+  // WAJIB: guna URL script.googleusercontent.com (CORS-friendly untuk GitHub Pages)
+  API_BASE: "https://script.googleusercontent.com/a/macros/pms.edu.my/echo?user_content_key=AY5xjrSFf5WPo7yioSZ5rIo5kJc8VkRTM9TAYm9X8avwbUwkdttGUljOc8yqU-ENczzo0Og1NGqAqi3oBjsZlPhACMXrEEb89WwiDtZ02-7qcUPbWefKZYq0KJPgYHpyeHF9W9IRRgFMMeW-WYqJM9ch6QNgjK6lNuEgyq9hhLZqV0HrmmRAjD5O-CRcvFRc6mCVThjSChKyoOWbn2NLbHb9SRqEZo5VozkVY5pzEbVipjzy-vzFDmK0mW-YLsAlhzwWCIoYwli5MB3gtQhhfLuFlGa9OJ8XCBIz44G16jqpoQYFBm6VO_f1fz2-H-5ZC78saa-laV3h&lib=MA8fNQb5aOeUkxow5nVjBtXloVKZ4aLC_",
 
   GOOGLE_CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com",
   ALLOWED_DOMAIN: "pms.edu.my"
