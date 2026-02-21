41702b63d7dd7c6c055032e9c661043c32188662
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-21 15:01:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 // config.js
 window.APP_CONFIG = {
-  // WAJIB: guna URL script.googleusercontent.com (CORS-safe)
-  API_BASE: "https://script.googleusercontent.com/a/macros/pms.edu.my/echo?user_content_key=AY5xjrSQllc5VbmeHXqAQaB0QllHzUJn5arDSz-hx4rkjXcQ9q5dbW0mf1rmhnb3lQ2zhN1pL9mn6Jx3YiF3RHD5sNuAFXdHGnc7HmTjgWle0SRDJQHg_YmiHV0XDYhSVorVjKBzV03K1ZomR3qtKsaKIhGEekZ0Rd0PFpUJx1k8yYLpG-D6I5Axou6Ttx_FDMVziM7kw7_XbaqTbF9aqlTfq1swyyiJaUgQbZQ_U0nTQNU9O95pJjbQ-LvfxMtFvEd9fFFLStFFGTSZNWqOTY5B_Fb45k2gW10tSWRXdPtaZV4yWLOg7DolDVUmm2_qsI63WiZbdHrY&lib=MA8fNQb5aOeUkxow5nVjBtXloVKZ4aLC_",
-
-  GOOGLE_CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com",
+  // guna Web App URL (script.google.com/macros/s/.../exec)
+  API_BASE: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
   ALLOWED_DOMAIN: "pms.edu.my"
 };
