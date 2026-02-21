72d69f5fafbd7551af0ac74a6e2267f607dd00b8
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-21 16:27:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 // config.js
 window.APP_CONFIG = {
-  // Apps Script Web App URL (exec) - proxy iframe will load this
-  PROXY_URL: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
+  // Letak URL googleusercontent yang kau test tadi (yang keluar JSON OK)
+  // Contoh: https://script.googleusercontent.com/a/macros/pms.edu.my/echo?user_content_key=...&lib=...
+  API_BASE: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
 
-  // Google OAuth Client ID (Web application client)
+  // Ini mesti client id sebenar dari Google Cloud Console (OAuth 2.0 Client ID)
+  // Format dia macam: 1234567890-xxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com
   GOOGLE_CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com",
 
-  // Domain yang dibenarkan
-  ALLOWED_DOMAIN: "pms.edu.my",
-
-  // Origin GitHub (untuk postMessage security)
-  GITHUB_ORIGIN: "https://kurakurahijau-z.github.io"
+  // Domain restriction
+  ALLOWED_DOMAIN: "pms.edu.my"
 };
