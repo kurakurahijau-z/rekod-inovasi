56f99d8af6dc734edc5fbc219c3e149cf4a5c76e
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-21 15:09:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 // config.js
 window.APP_CONFIG = {
-  // guna Web App URL (script.google.com/macros/s/.../exec)
-  API_BASE: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
+  // WAJIB: guna URL script.googleusercontent.com (CORS-safe)
+  // Letak URL echo yang terbaru (yang kau bagi last sekali)
+  API_BASE: "https://script.googleusercontent.com/a/macros/pms.edu.my/echo?user_content_key=AY5xjrR0ivFaRgDrEuiSKVPSqAvP-LncD1dcngN19Hnn024sBv08pt7P53DnVJot8UTuSnsfvORzS7BwXsRQfQo2INuUkDRxgRVsTA3Aw_lnyFie3rf9h4JsAqMY71X5LxQ5K36VfzlKknhI2S_fToibscP88IpO_yAY8hquLFZhjb3Dl1iBz3_KC9JnpnejsBNUY8lsP_7SN27utpyTXVKTBwgzJ_K2ZjXYJIZyhEcbSaxZlHtr3SpkMlh5gXEhdBcu8lCSGQipcLz3z2StEqrhrw5y7qE_ZApl2aly6Meq7H4uLQL9iENva5F0aDWKSd3hFLMXis8k&lib=MA8fNQb5aOeUkxow5nVjBtXloVKZ4aLC_",
+
+  // Client ID OAuth yang kau guna dalam Google Identity Services
+  GOOGLE_CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidsed7bp123m.apps.googleusercontent.com",
+
+  // Domain yang dibenarkan
   ALLOWED_DOMAIN: "pms.edu.my"
 };
