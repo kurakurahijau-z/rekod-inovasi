9ca2b4c664877d5596eb85e6e0809b5236220572
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-21 14:19:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 window.APP_CONFIG = {
-  // WAJIB: guna URL script.googleusercontent.com (CORS-friendly untuk GitHub Pages)
-  API_BASE: "https://script.googleusercontent.com/a/macros/pms.edu.my/echo?user_content_key=AY5xjrSFf5WPo7yioSZ5rIo5kJc8VkRTM9TAYm9X8avwbUwkdttGUljOc8yqU-ENczzo0Og1NGqAqi3oBjsZlPhACMXrEEb89WwiDtZ02-7qcUPbWefKZYq0KJPgYHpyeHF9W9IRRgFMMeW-WYqJM9ch6QNgjK6lNuEgyq9hhLZqV0HrmmRAjD5O-CRcvFRc6mCVThjSChKyoOWbn2NLbHb9SRqEZo5VozkVY5pzEbVipjzy-vzFDmK0mW-YLsAlhzwWCIoYwli5MB3gtQhhfLuFlGa9OJ8XCBIz44G16jqpoQYFBm6VO_f1fz2-H-5ZC78saa-laV3h&lib=MA8fNQb5aOeUkxow5nVjBtXloVKZ4aLC_",
+  // WAJIB: guna URL script.googleusercontent.com (CORS-safe)
+  API_BASE: "https://script.googleusercontent.com/a/macros/pms.edu.my/echo?user_content_key=AY5xjrSQllc5VbmeHXqAQaB0QllHzUJn5arDSz-hx4rkjXcQ9q5dbW0mf1rmhnb3lQ2zhN1pL9mn6Jx3YiF3RHD5sNuAFXdHGnc7HmTjgWle0SRDJQHg_YmiHV0XDYhSVorVjKBzV03K1ZomR3qtKsaKIhGEekZ0Rd0PFpUJx1k8yYLpG-D6I5Axou6Ttx_FDMVziM7kw7_XbaqTbF9aqlTfq1swyyiJaUgQbZQ_U0nTQNU9O95pJjbQ-LvfxMtFvEd9fFFLStFFGTSZNWqOTY5B_Fb45k2gW10tSWRXdPtaZV4yWLOg7DolDVUmm2_qsI63WiZbdHrY&lib=MA8fNQb5aOeUkxow5nVjBtXloVKZ4aLC_",
 
   GOOGLE_CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com",
   ALLOWED_DOMAIN: "pms.edu.my"
