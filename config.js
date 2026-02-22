72ebce48d632634ebd9b3a6de4e1770c6ed8bb9c
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-22 15:59:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
+// config.js
 window.APP_CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
+  // IMPORTANT: guna googleusercontent (CORS-safe), BUKAN /exec
+  API_BASE: "https://script.googleusercontent.com/a/macros/pms.edu.my/echo?user_content_key=AY5xjrQJgdTH0tWjy60xX_nGmF96NpR3eNyN3XkCi5d88mtli2qsnbIVpNqlrGc8QJJc45nV0rxGzvnyQDzoIkvv384iPN8HWHxfCLWkUF1BKZpCWULT-i84O-zqxSBJpRdRo87ohmNua8vwT7lZ792mZ4_hDEw9z0JdbRpZnUh5Z-8mu7fTX1POQ3dGodhXLvPqn8F3OqaNjTH3ovW3fp-2eHsJObm0nMa-PJta3ZYSn2l_PU8FKdIXb47zfYUBTvEAxlG1eYBCw0Q0jI-h4WF0Jm0sSj94By0jwKU5xSVgFLTjv39n3Tc&lib=MA8fNQb5aOeUkxow5nVjBtXloVKZ4aLC_",
+
   GOOGLE_CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com",
   ALLOWED_DOMAIN: "pms.edu.my"
 };
