0b6c6c9064265d1631d6cc40803d357d899910b5
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-22 14:15:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// config.js
 window.APP_CONFIG = {
   API_BASE: "https://script.google.com/macros/s/AKfycbw5B9rmptyZuBJ7nIf1rHZBA7in4emEJF2Ubaep0pFDerua6APFXsoU_XdJpyhuy7KO/exec",
   GOOGLE_CLIENT_ID: "637964120539-4knkg8lrjdaludsn8gncjidse7bpl23m.apps.googleusercontent.com",
